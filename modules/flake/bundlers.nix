dbbc34ef37752856e42de198311db2463ed21b2c
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-08-11 09:59:49 -0500

    chore(deps): update core.nix lock inputs

--- modules/flake/bundlers.nix
+++ modules/flake/bundlers.nix
@@ -17,12 +17,15 @@ let
     };
   };
 
-  inherit (inputs.self.components) nixology;
   inherit (inputs.core.inputs) flake-parts;
   inherit (config.partitions.schemas.extraInputs) flake-schemas;
 in
 lib.mkComponent {
   name = lib.basename __curPos.file;
   modules = { inherit flake; };
-  dependencies = [ nixology.core.transposition ];
+  dependencies = with inputs.self.components; [ nixology.core.transposition ];
+  meta = {
+    description = "Bundlers for flake-parts";
+    shortDescription = "Bundlers for flake-parts";
+  };
 }
