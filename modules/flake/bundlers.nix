fb2ed34af54f06f54c2b4e075d99ef647052a9c9
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-08-12 11:48:36 -0500

    chore: fix

--- modules/flake/bundlers.nix
+++ modules/flake/bundlers.nix
@@ -22,8 +22,11 @@ let
 in
 lib.mkComponent {
   name = lib.basename __curPos.file;
+
   modules = { inherit flake; };
+
   dependencies = with inputs.self.components; [ nixology.core.transposition ];
+
   meta = {
     description = "Bundlers for flake-parts";
     shortDescription = "Bundlers for flake-parts";
